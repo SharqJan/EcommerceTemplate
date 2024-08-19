9a255218f374acd78f3d0d855ed0a3a67919a8fe
SharQ Jan <117840398+SharqJan@users.noreply.github.com>
2024-08-19 17:19:23 +0530

    initial commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add(object? productModel)
+        {
+            return View();
+        }
+
+        public IActionResult Detail(int id)
+        {
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            return RedirectToAction("Add", new
+            {
+                Id = 100
+            });
+        }
+    }
+}
